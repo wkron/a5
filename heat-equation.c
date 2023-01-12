4335efaeb5b0ea5eb7f3316e4466b8554d41d44d
William Kronbæk <wkronbaek@gmail.com>
2023-01-12 09:44:16 +0100

    parallelisering mangler

--- heat-equation.c
+++ heat-equation.c
@@ -3,27 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <omp.h>
+#include "omp.h"
 #include "debugbmp.h"
 
 size_t pos(size_t width, size_t x, size_t y) {
-    assert(0);
+    return y*width + x;
 }
 
 void write_borders(float* data, size_t width, size_t height) {
-    assert(0);
+    for (size_t n = 0; n<width; n++){
+        data[pos(width, n,0)] = 20.0;
+        data[pos(width, n,height-1)] = -273.15;
+    }
+    for (size_t n = 0; n<height; n++){
+        data[pos(width, 0,n)] = -273.15;
+        data[pos(width, width-1,n)] = -273.15;
+    }
 }
 
 float stencil(float* data, size_t width, size_t x, size_t y, float alpha) {
-    assert(0);
+    return alpha * (data[pos(width, x,y)] + data[pos(width, x-1,y)] + data[pos(width, x+1,y)] + data[pos(width, x,y-1)] + data[pos(width, x,y+1)]);
 }
 
 void apply_stencil(float* data, size_t width, size_t height, size_t offset, float alpha) {
-    assert(0);
+    for (size_t x = 1; x < width-1; x++){
+        for (size_t y = 1 + ((x+offset)%2); y< height-1; y+=2){
+            data[pos(width, x, y)] = stencil(data, width, x, y, alpha);
+        }
+    }
 }
 
 float compute_delta(float* data, float* prev, size_t width, size_t height) {
-    assert(0);
+    float res = 0.0;
+    for (size_t x = 0; x < width; x++){
+        for (size_t y = 0; y < height; y++){
+            res += fabs(prev[pos(width, x, y)]-data[pos(width, x, y)]);
+        }
+    }
+
+    return res / ((float) width*height);
 }
 
 void run_simulation(size_t width, size_t height, size_t steps, const char* filename) {
